2b541ae53f14e0228e01b1282dd592bbb39e9615
company <you@example.com>
2020-08-05 09:35:33 +0000

    3333

---
+++ resources/js/src/ExampleComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                    <div>
+                        {{message}}
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <a-button type="primary">按钮</a-button>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'hello laravel-vue'
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+</head>
+<body>
+
+<div id="app">
+    <admin-layout/>
+</div>
+
+</body>
+</html>
